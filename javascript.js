b8f13036aab3767aa778b2a8133bff71203397df
Dtdeep <dumagandesthiny@gmail.com>
2026-01-29 09:14:40 +0800

    Change the design and add a button for new grid

--- javascript.js
+++ javascript.js
@@ -1,12 +1,22 @@
 const mainContainer = document.querySelector(".main-container");
-for(let i = 0; i < 256; i++){
-    let divElement = document.createElement("div");
+const newGridBtn = document.querySelector("#newGirdBtn");
+let newGridDimension = 256;
+
+const newGridBtnListener = newGridBtn.addEventListener("click", ()=>{
+    let newGridPrompt = prompt("What will be the new dimension?", 16);
+        newGridDimension = newGridPrompt * newGridPrompt;
+        console.log("it works!");
+});
+
+
+for(let i = 0; i < newGridDimension; i++){
+    const divElement = document.createElement("div");
     divElement.classList.add("div-Element");
     mainContainer.appendChild(divElement);
 }
 
 mainContainer.addEventListener("mouseover", (event) => {
-    let eventTarget = event.target;
+    const eventTarget = event.target;
     console.log(eventTarget);  
     if(eventTarget.classList == "div-Element"){
         eventTarget.classList.add("new-color")
