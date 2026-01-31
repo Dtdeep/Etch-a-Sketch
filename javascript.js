c98a61f747f25bc1c4a14cec028c8f6657dccc4b
Dtdeep <dumagandesthiny@gmail.com>
2026-01-31 19:48:37 +0800

    Make user input reflect the actual dimension of the boxes

--- javascript.js
+++ javascript.js
@@ -1,16 +1,6 @@
 const mainContainer = document.querySelector(".main-container");
 const newGridBtn = document.querySelector("#newGridBtn");
 
-const createGrid = (dimension) =>{
-    removeCurrentGrid();
-    numberOfBoxes = dimension * dimension;
-    for(let i = 0; i < numberOfBoxes; i++){
-        const divElement = document.createElement("div");
-        divElement.classList.add("div-Element");
-        mainContainer.appendChild(divElement);
-    }
-}
-
  const removeCurrentGrid = () =>{
     const allDivElement = document.querySelectorAll(".div-Element");
     let allDivElementArray = [...allDivElement];
@@ -20,24 +10,42 @@ const createGrid = (dimension) =>{
     })  
  }
 
+const createNewGrid = (dimension) =>{
+    removeCurrentGrid();
+    const numberOfTotalDivs = dimension * dimension;
+    const sizeForEachDiv = 960/dimension;
+    for(let i = 0; i < numberOfTotalDivs; i++){
+        const divElement = document.createElement("div");
+        divElement.classList.add("div-Element");
+        divElement.style.width = `${sizeForEachDiv}px`;
+        divElement.style.height = `${sizeForEachDiv}px`;
+        mainContainer.appendChild(divElement);
+    }
+}
 
-
-const newGridPromptVar = () => {
-    let newGridPrompt = prompt("What will be the new dimension?", 16);
-    createGrid(newGridPrompt);
-    console.log("it works!");
+const askNewGridDimensionPromp = () => {
+    const newGridSizePrompt = prompt("What will be the new dimension?", 16);
+    const isWithin100 = newGridSizePrompt <= 100
+    const isGreaterThan0 = newGridSizePrompt > 0;
+    if(isWithin100 && isGreaterThan0){
+        createNewGrid(newGridSizePrompt);
+    }else{
+        alert("Too big or too small or not allowed");
+    }
 };
 
-mainContainer.addEventListener("mouseover", (event) => {
+const changeColorWhenHovered = (event) => {
     const eventTarget = event.target;
     console.log(eventTarget);  
     if(eventTarget.classList == "div-Element"){
         eventTarget.classList.add("new-color")
     }
-});
+}
 
-createGrid(16);
-newGridBtn.addEventListener("click", newGridPromptVar);
+mainContainer.addEventListener("mouseover", changeColorWhenHovered);
+
+createNewGrid(16);
+newGridBtn.addEventListener("click", askNewGridDimensionPromp);
 
 
 
