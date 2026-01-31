521875c369c6e1d4ba9f0c52760d92b0b26ed3cd
Dtdeep <dumagandesthiny@gmail.com>
2026-01-31 11:10:43 +0800

    Add removeCurrentDiv function
    it doesnt work yet

--- javascript.js
+++ javascript.js
@@ -2,13 +2,22 @@ const mainContainer = document.querySelector(".main-container");
 const newGridBtn = document.querySelector("#newGridBtn");
 
 const createGrid = (dimension) =>{
+    removeCurrentGrid();
     numberOfBoxes = dimension * dimension;
     for(let i = 0; i < numberOfBoxes; i++){
         const divElement = document.createElement("div");
         divElement.classList.add("div-Element");
         mainContainer.appendChild(divElement);
-    };
-};
+    };()=>{
+
+  node  }
+}
+
+ const removeCurrentGrid = () =>{
+    const allDivElement = document.querySelectorAll("#div-Element");
+ }
+
+
 
 const newGridPromptVar = () => {
     let newGridPrompt = prompt("What will be the new dimension?", 16);
